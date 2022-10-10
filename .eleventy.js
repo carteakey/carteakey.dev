4c7fbcfff51b0b4091567fb4715d15c91986549a
Kartikey Chauhan <kartikeychauhan27@gmail.com>
2022-10-10 17:14:44 -0400

    google font plugin

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ const markdownItAnchor = require("markdown-it-anchor");
 
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
+const eleventyGoogleFonts = require("eleventy-google-fonts");
 
 module.exports = function (eleventyConfig) {
   // Copy the `img` and `css` folders to the output
@@ -15,6 +16,7 @@ module.exports = function (eleventyConfig) {
   // Add plugins
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(eleventyGoogleFonts);
 
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
