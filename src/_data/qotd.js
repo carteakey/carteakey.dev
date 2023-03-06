d7d248ead722a5132b63f5841582d819d6320785
Kartikey Chauhan <kartikeychauhan27@gmail.com>
2023-03-05 22:45:59 -0500

    chatgpt powered quote lmao this is my peak

---
+++ src/_data/qotd.js
@@ -0,0 +1,51 @@
+const { AssetCache } = require("@11ty/eleventy-fetch");
+
+module.exports = async function() {
+
+    let qotd = new AssetCache("qotd");
+        
+    if(qotd.isCacheValid("1d")) {
+        // return cached data.
+        return qotd.getCachedValue(); // a promise
+    }
+
+    try {
+  
+        const { Configuration, OpenAIApi } = require("openai");
+
+        const configuration = new Configuration({
+            apiKey: process.env.OPENAI_API_KEY
+        });
+    
+        const openai = new OpenAIApi(configuration);
+    
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {role: "user", content: "Tell me a rare inspirational quote and its author"}],
+        });
+        
+        quote = {
+            "quote": completion.data.choices[0].message.content
+        }
+
+        console.log(quote);
+
+        await qotd.save(quote, "json");
+
+        return quote
+
+    } 
+    catch(e) 
+    {   
+        console.log(e)
+        return {
+        // my failure fallback data
+            "quote": "The illiterate of the 21st century will not be those who cannot read and write, but those who cannot learn, unlearn, and relearn. - Alvin Toffler"
+        }
+    }
+
+//   return {
+//     stargazers: json.stargazers_count
+//   };
+};
