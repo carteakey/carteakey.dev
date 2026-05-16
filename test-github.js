72e06c0c6d7456f5afb6cc5097886668b5ae09c8
Kartikey Chauhan <kartikeychauhan27@gmail.com>
2026-05-16 18:58:44 -0400

    chore(utils): remove stale scripts

--- test-github.js
+++
@@ -1,9 +0,0 @@
-import g from './src/_data/github.js';
-import gc from './src/_data/github-card.js';
-async function run() {
-  const dataG = await g();
-  console.log("github.js repos count:", dataG.repos.length);
-  const dataGc = await gc();
-  console.log("github-card.js repoCount:", dataGc.repoCount);
-}
-run();
--- test-github3.js
+++
@@ -1,6 +0,0 @@
-import gc from './src/_data/github-card.js';
-async function run() {
-  const dataGc = await gc();
-  console.log("github-card.js repoCount:", dataGc.repoCount);
-}
-run();
